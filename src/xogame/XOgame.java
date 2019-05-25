781776a2ecd4c91206400c0bd491b361f07dac86
javcoder <engineer.ahmedtalal@gmail.com>
2019-05-25 16:21:42 +0200

    first app

---
+++ src/xogame/XOgame.java
@@ -0,0 +1,13 @@
+package xogame;
+
+import XO_Gui.XOLoadingFrame;
+import XO_Gui.XOgameFrame;
+
+public class XOgame {
+
+    public static void main(String[] args) {
+       
+       new XOLoadingFrame().setVisible(true);
+    }
+    
+}
